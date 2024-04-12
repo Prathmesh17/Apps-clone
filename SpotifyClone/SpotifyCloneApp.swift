c458ecafb6d9d8f25398f195f428d0abc8e711e3
Prathmesh <prathmeshparteki@gmail.com>
2024-04-12 23:39:54 +0530

    Initial Commit

---
+++ SpotifyClone/SpotifyCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyCloneApp.swift
+//  SpotifyClone
+//
+//  Created by Prathmesh Parteki on 12/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
